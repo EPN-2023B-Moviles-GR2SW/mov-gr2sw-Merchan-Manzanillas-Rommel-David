039e4addb819ed123a7e0319ab1c1480cd18e3b7
youRollex <rommel.merchan@epn.edu.ec>
2023-12-03 21:18:33 -0500

    primerContacto con AndroidStudio

--- 02 - Android/app/src/main/java/com/example/b2023gr2sw01/MainActivity.kt
+++ 02 - Android/app/src/main/java/com/example/b2023gr2sw01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.b2023gr2sw01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,35 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.b2023gr2sw01.ui.theme.B2023gr2sw01Theme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            B2023gr2sw01Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    B2023gr2sw01Theme {
-        Greeting("Android")
-    }
-}
